42575037fd21cc824e061c529ec41525bb02fbaf
tixuz <hindolmirzo@gmail.com>
2016-06-28 04:40:19 +0500

    I've just created a new Java project

---
+++ lesson20egit/src/uz/tix/lesson20egit/Main.java
@@ -0,0 +1,10 @@
+package uz.tix.lesson20egit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git Hubdan!!!");
+	}
+
+}
